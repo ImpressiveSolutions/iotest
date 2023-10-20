eaf01c6aadb0fb6a62fb8f5fda03a5c7f1a06026
ImpressiveSolutions <82044151+ImpressiveSolutions@users.noreply.github.com>
2023-10-21 01:50:27 +0300

    Born: iotest.sh

---
+++ iotest.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+# BS made by me :-)
+trap _exit INT QUIT TERM
+
+_red() {
+    printf '\033[0;31;31m%b\033[0m' "$1"
+}
+
+_green() {
+    printf '\033[0;31;32m%b\033[0m' "$1"
+}
+
+_yellow() {
+    printf '\033[0;31;33m%b\033[0m' "$1"
+}
+
+_blue() {
+    printf '\033[0;31;36m%b\033[0m' "$1"
+}
+
+_exists() {
+    local cmd="$1"
+    if eval type type >/dev/null 2>&1; then
+        eval type "$cmd" >/dev/null 2>&1
+    elif command >/dev/null 2>&1; then
+        command -v "$cmd" >/dev/null 2>&1
+    else
+        which "$cmd" >/dev/null 2>&1
+    fi
+    local rt=$?
+    return ${rt}
+}
+
+_exit() {
+    _red "\nThe script has been terminated. Cleaning up files...\n"
+    rm -fr benchtest_*
+    exit 1
+}
+
+next() {
+    printf "%-70s\n" "-" | sed 's/\s/-/g'
+}
+
+io_test() {
+    (LANG=C dd if=/dev/zero of=benchtest_$$ bs=512k count=$1 conv=fdatasync && rm -f benchtest_$$) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+}
+
+calc_size() {
+    local raw=$1
+    local total_size=0
+    local num=1
+    local unit="KB"
+    if ! [[ ${raw} =~ ^[0-9]+$ ]]; then
+        echo ""
+        return
+    fi
+    if [ "${raw}" -ge 1073741824 ]; then
+        num=1073741824
+        unit="TB"
+    elif [ "${raw}" -ge 1048576 ]; then
+        num=1048576
+        unit="GB"
+    elif [ "${raw}" -ge 1024 ]; then
+        num=1024
+        unit="MB"
+    elif [ "${raw}" -eq 0 ]; then
+        echo "${total_size}"
+        return
+    fi
+    total_size=$(awk 'BEGIN{printf "%.1f", '$raw' / '$num'}')
+    echo "${total_size} ${unit}"
+}
+
+print_io_test() {
+    freespace=$(df -m . | awk 'NR==2 {print $4}')
+    if [ -z "${freespace}" ]; then
+        freespace=$(df -m . | awk 'NR==3 {print $3}')
+    fi
+    if [ ${freespace} -gt 1024 ]; then
+        writemb=2048
+        io1=$(io_test ${writemb})
+        echo " I/O Speed(1st run) : $(_yellow "$io1")"
+        io2=$(io_test ${writemb})
+        echo " I/O Speed(2nd run) : $(_yellow "$io2")"
+        io3=$(io_test ${writemb})
+        echo " I/O Speed(3rd run) : $(_yellow "$io3")"
+        ioraw1=$(echo $io1 | awk 'NR==1 {print $1}')
+        [ "$(echo $io1 | awk 'NR==1 {print $2}')" == "GB/s" ] && ioraw1=$(awk 'BEGIN{print '$ioraw1' * 1024}')
+        ioraw2=$(echo $io2 | awk 'NR==1 {print $1}')
+        [ "$(echo $io2 | awk 'NR==1 {print $2}')" == "GB/s" ] && ioraw2=$(awk 'BEGIN{print '$ioraw2' * 1024}')
+        ioraw3=$(echo $io3 | awk 'NR==1 {print $1}')
+        [ "$(echo $io3 | awk 'NR==1 {print $2}')" == "GB/s" ] && ioraw3=$(awk 'BEGIN{print '$ioraw3' * 1024}')
+        ioall=$(awk 'BEGIN{print '$ioraw1' + '$ioraw2' + '$ioraw3'}')
+        ioavg=$(awk 'BEGIN{printf "%.1f", '$ioall' / 3}')
+        echo " I/O Speed(average) : $(_yellow "$ioavg MB/s")"
+    else
+        echo " $(_red "Not enough space for I/O Speed test!")"
+    fi
+}
+
+print_end_time() {
+    end_time=$(date +%s)
+    time=$((${end_time} - ${start_time}))
+    if [ ${time} -gt 60 ]; then
+        min=$(expr $time / 60)
+        sec=$(expr $time % 60)
+        echo " Finished in        : ${min} min ${sec} sec"
+    else
+        echo " Finished in        : ${time} sec"
+    fi
+    date_time=$(date '+%Y-%m-%d %H:%M:%S %Z')
+    echo " Timestamp          : $date_time"
+}
+
+! _exists "wget" && _red "Error: wget command not found.\n" && exit 1
+! _exists "free" && _red "Error: free command not found.\n" && exit 1
+
+start_time=$(date +%s)
+clear
+next
+print_io_test
+next
+print_end_time
+next
